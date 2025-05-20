e57cbd439c1371fbc60f84675015d03584d53ec1
AES MARKETING LTDA <infra@aesmkt.com.br>
2025-05-20 14:01:10 -0300

    Add files via upload

---
+++ app/layout.tsx
@@ -0,0 +1,25 @@
+import type React from "react"
+import type { Metadata } from "next"
+import { Inter } from "next/font/google"
+import "./globals.css"
+
+const inter = Inter({ subsets: ["latin"] })
+
+export const metadata: Metadata = {
+  title: "The Voos - Agência de Viagens",
+  description:
+    "Viagens nacionais e internacionais com os melhores preços. Pacotes, passagens aéreas, hotéis e muito mais!",
+    generator: 'v0.dev'
+}
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode
+}>) {
+  return (
+    <html lang="pt-BR">
+      <body className={inter.className}>{children}</body>
+    </html>
+  )
+}
